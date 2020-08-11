90d633b394487136006d8b1557302e6e91a24f84
gardenjw28 <hhshin1228@naver.com>
2020-08-11 15:17:06 +0900

    Create 2019.더블더블.py

---
+++ 2019.더블더블.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+n = 1
+i = int(input())
+print(n, end=' ')
+for _ in range(i):
+    n = n*2
+    print(n, end=' ')
+
+
+# In[7]:
+
+
+n = int(input())
+for i in range(0, n+1):
+    print(2**i, end = ' ')
+
+
+# In[ ]:
+
+
+
+
